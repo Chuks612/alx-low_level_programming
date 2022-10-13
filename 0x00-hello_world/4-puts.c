e5b7e995e02044fc22778c29a502e0348f9dd8a5
Chuks612 <Chuks612@gmail.com>
2022-10-13 15:24:19 -0700

    another commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  *  * main - print the string in the put function
- *  * 
+ *  *
  *  * Description: using the main function
  *  * this program prints "Programming is like building a multilingual puzzle
  *  * Return: 0
@@ -10,5 +10,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return(0);
+		return (0);
 }
