74ad4ed6bfd9ecee181c2fc8a68c4984f539167a
Chuks612 <Chuks612@gmail.com>
2022-10-18 15:17:34 -0700

    another commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print _sign - prints the sign of a nmuber
+ * @n: number being tested 
+ * Return: always 0
+ */
+
+int prin_sign(int n)
+{
+	if( n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('_');
+		return (-1);
+	}
+	return (0);
+}
